206882973c4feb89d49bd43481839d7bd4799bf5
EduardoMendo <efmendoza@uamv.edu.ni>
2024-10-14 00:33:00 -0600

    Agregue clase student

--- example_persistence/src/main/java/Main.java
+++ example_persistence/src/main/java/Main.java
@@ -1,12 +1,24 @@
 import dao.CityDao;
+import dao.StudentDao;
 import models.City;
+import models.Student;
 
 public class Main {
     public static void main(String[] args) {
         CityDao daoCity = new CityDao();
+        StudentDao daoStudent = new StudentDao();
         City city = new City();
+        Student student = new Student();
 
         city.setName("Managua");
+        city.setName("Masaya");
         daoCity.save(city);
+
+        student.setFirstName("Eduardo");
+        student.setLastName("Mendoza");
+        student.setEmail("eduardo@gmail.com");
+        student.setCif("22020417");
+        daoStudent.save(student);
+
     }
 }
